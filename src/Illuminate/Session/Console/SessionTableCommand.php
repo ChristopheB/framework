f38430c22dc61ca824feebfcab2a38dfe2c6d8ba
Tom Lunar <tomlunar@outlook.com>
2014-03-22 02:40:53 +0100

    Make the migrations path configurable.

--- src/Illuminate/Session/Console/SessionTableCommand.php
+++ src/Illuminate/Session/Console/SessionTableCommand.php
@@ -62,7 +62,7 @@ class SessionTableCommand extends Command {
 	{
 		$name = 'create_session_table';
 
-		$path = $this->laravel['path'].'/database/migrations';
+		$path = $this->laravel['path.migrations'];
 
 		return $this->laravel['migration.creator']->create($name, $path);
 	}
